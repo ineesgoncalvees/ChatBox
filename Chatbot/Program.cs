6f580d3ac4b909684911e9a9b604f7d6c102b760
Ines <ines98goncalves@gmail.com>
2018-03-01 11:17:10 +0000

    Codigo terminado

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -7,6 +7,42 @@ using System.Threading.Tasks;
 namespace Chatbot {
     class Program {
         static void Main(string[] args) {
+
+            string pergunta, resposta;
+
+            do {
+                Console.WriteLine("Pergunta-me alguma coisa");
+
+                pergunta = Console.ReadLine();
+
+                switch (pergunta) {
+                    case "Como e' que te chamas?":
+                        resposta = "Antonio Jose, Toze para os amigos.";
+                        Console.WriteLine(resposta);
+                        break;
+                    case "Como estas?":
+                        resposta = "Estou bem obrigado.";
+                        Console.WriteLine(resposta);
+                        break;
+                    case "Quantos anos tens?":
+                        resposta = "Nao me parece que seja da tua conta.";
+                        Console.WriteLine(resposta);
+                        break;
+                    case "Qual é a tua comida preferida?":
+                        resposta = "Por mim comia pizza e nuggets todos os dias :^)";
+                        Console.WriteLine(resposta);
+                        break;
+                    case "exit":
+                        resposta = "Fechar programa";
+                        Console.WriteLine(resposta);
+                        continue;
+                    default:
+                        resposta = "Nao tenho resposta para tal pergunta...";
+                        Console.WriteLine(resposta);
+                        break;
+                }
+
+            } while (pergunta != "exit");            
         }
     }
 }
